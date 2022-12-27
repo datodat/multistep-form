acbfe50cfd4d59da9497d30ca091ce8c4f4c1d77
datodat <dato.kevlishvili.1990@gmail.com>
2022-12-28 01:04:22 +0400

    info page

--- src/App.js
+++ src/App.js
@@ -10,15 +10,24 @@ import SummaryPage from "./components/Pages/SummaryPage/SummaryPage";
 
 const App = () => {
   const [page, setPage] = useState(1);
+  const [pageData, setPageData] = useState({});
 
   const pageHandler = p => {
     setPage(p);
   }
 
+  const infoPageHandler = infoData => {
+    setPageData({ ...pageData, infoData });
+    window.localStorage.setItem('name', infoData.name);
+    window.localStorage.setItem('email', infoData.email);
+    window.localStorage.setItem('phone', infoData.phone);
+    setPage(2);
+  }
+
   return (
     <div className="container">
       <Card pageHandler={pageHandler} activePage={page}>
-        {page === 1 && <InfoPage />}
+        {page === 1 && <InfoPage infoPageHandler={infoPageHandler} />}
         {page === 2 && <PlanPage />}
         {page === 3 && <AddonsPage />}
         {page === 4 && <SummaryPage />}
